f056d9158970a6d1bc907262dfe1c804a343e98f
MohamedALMAHMOUD <contact@cabane-maths.fr>
2023-07-24 17:54:27 +0200

    modification of url cors

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from passeword import passeword
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://mohamedalmahmoud.github.io/portfolio/templates/competences.html"}})
+CORS(app, resources={r"/api/*": {"origins": "https://mohamedalmahmoud.github.io"}})
 
 
 @app.route('/')
